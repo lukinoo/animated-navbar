2be5fce218e1bcfa19c6ef14707900944980c28b
luka <luka.bliadze.3@iliauni.edu.ge>
2021-01-01 05:13:46 -0800

    start's here

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// const navLinks = document.querySelector('.nav-links');
+// const hamburger = document.querySelector('.burger');
+
+// hamburger.addEventListener('click', () => {
+//     navLinks.classList.toggle('open');
+// })
